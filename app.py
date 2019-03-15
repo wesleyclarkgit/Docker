4400eeb3510e7557f17991df3855b937d30ca8de
Wesley Clark <iamwesleyclark@gmail.com>
2019-03-14 22:56:59 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(bytes("Hello from Python!", "utf8"))
+        return
+
+def run():
+    httpd = HTTPServer(('0.0.0.0', 8080), testHTTPServer_RequestHandler)
+    httpd.serve_forever()
+
+run()
